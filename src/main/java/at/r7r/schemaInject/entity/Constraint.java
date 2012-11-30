ed8e9cf2194e750637838e4a3803af420f580f4f
Manuel Reithuber <git@manuel.reithuber.net>
2012-11-30 06:02:49 +0100

    Constraint: added helper methods getFields() and getFieldCount()

--- src/main/java/at/r7r/schemaInject/entity/Constraint.java
+++ src/main/java/at/r7r/schemaInject/entity/Constraint.java
@@ -1,5 +1,40 @@
 package at.r7r.schemaInject.entity;
 
-public class Constraint {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Base class for all constraints (PrimaryKey, ForeignKey, Unique, Check)
+ * @author Manuel Reithuber
+ */
+public class Constraint {
+	/**
+	 * Helper method to get the field count for values which can be described using either an attribute or a tag list
+	 * @param attr XML attribute value
+	 * @param tagList XML tag list
+	 * @return Field count (if attr != null, tagList will be ignored and 1 will be returned)
+	 */
+	protected static int getFieldCount(String attr, List<String> tagList) {
+		int rc = 0;
+		if (attr != null) rc = 1;
+		else if (tagList != null) rc = tagList.size();
+		return rc;
+	}
+
+	/**
+	 * Helper method to get the fields for values which can be described using either an attribute or a tag list
+	 * @param attr XML attribute value
+	 * @param tagList XML tag list
+	 * @return Fields (if attr != null, it returns a new list with just that one value)
+	 */
+	protected static List<String> getFields(String attr, List<String> tagList) {
+		List<String> rc;
+		if (attr != null) {
+			rc = new ArrayList<String>();
+			rc.add(attr);
+		}
+		else rc = tagList;
+
+		return rc;
+	}
 }
