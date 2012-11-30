e4d0144254572db6c68f54cdcba8d7829a908613
Manuel Reithuber <git@manuel.reithuber.net>
2012-11-30 06:06:13 +0100

    implemented ForeignKey

--- src/main/java/at/r7r/schemaInject/entity/ForeignKey.java
+++ src/main/java/at/r7r/schemaInject/entity/ForeignKey.java
@@ -1,5 +1,46 @@
 package at.r7r.schemaInject.entity;
 
-public class ForeignKey extends Constraint {
+import java.util.LinkedList;
+import java.util.List;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+
+@XStreamAlias("fkey")
+public class ForeignKey extends Constraint {
+	@XStreamAsAttribute
+	private String name = null;
+
+	@XStreamAsAttribute
+	@XStreamAlias("from")
+	private String fromField = null;
+
+	@XStreamAsAttribute
+	private String toTable = null;
+
+	@XStreamAsAttribute
+	private String toField = null;
+
+	@XStreamImplicit(itemFieldName="from")
+	private List<String> fromFields = new LinkedList<String>();
+
+	@XStreamImplicit(itemFieldName="to")
+	private List<String> toFields = new LinkedList<String>();
+
+	public String getName() {
+		return name;
+	}
+
+	public List<String> getFrom() {
+		return getFields(fromField, fromFields);
+	}
+
+	public String getToTable() {
+		return toTable;
+	}
+
+	public List<String> getTo() {
+		return getFields(toField, toFields);
+	}
 }
