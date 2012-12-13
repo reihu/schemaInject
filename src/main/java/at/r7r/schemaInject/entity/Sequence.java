802bd37fc9b5fb497873dc69982cacb697ec0756
Manuel Reithuber <git@manuel.reithuber.net>
2012-12-13 04:04:05 +0100

    added first (still untested) Sequence implementation

--- src/main/java/at/r7r/schemaInject/entity/Sequence.java
+++ src/main/java/at/r7r/schemaInject/entity/Sequence.java
@@ -1,8 +1,28 @@
 package at.r7r.schemaInject.entity;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
 @XStreamAlias("sequence")
 public class Sequence {
-
+	@XStreamAsAttribute
+	private String name = null;
+	
+	@XStreamAsAttribute
+	private long from = 0;
+	
+	@XStreamAsAttribute
+	private long step = 0;
+	
+	long getFrom() {
+		return from;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	long getStep() {
+		return step;
+	}
 }
